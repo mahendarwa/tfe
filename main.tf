852f3f4060046b272c63bd56faec112bd4c70b05
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2023-08-14 01:36:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,15 +3,8 @@ resource "null_resource" "my_hello_world1" {
     command = <<-EOT
       # Your local shell command here
       echo "Hello from local command" > local_output.txt
+      ls
     EOT
   }
 }
 
-data "local_file" "example" {
-  depends_on = [null_resource.my_hello_world1]  # If needed, specify dependencies
-  filename = "local_output.txt"  # Update the path accordingly
-}
-
-output "file_content" {
-  value = data.local_file.example.content
-}
