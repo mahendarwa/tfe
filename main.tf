ee5baf24278abb91723c365618a3d80c09c3ca3d
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2023-08-16 00:11:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "my_hello_worl12" {
+resource "null_resource" "my_hello_worl132" {
   provisioner "local-exec" {
     command = <<-EOT
       # Your local shell command here
