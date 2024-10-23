d19b034fede1e3906a4de1deba2f4710193b842d
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2024-10-23 15:51:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,15 +34,11 @@ jobs:
           echo "SUBSCRIPTION_ID=$(if [[ ${{ github.event.inputs.subenv }} == 'prod' ]]; then echo '5b02df11'; else echo 'e0e5bf14'; fi)" >> $GITHUB_ENV
           echo "RESOURCE_GROUP=crs-${{ github.event.inputs.subenv }}-shared-${{ github.event.inputs.location }}-rg" >> $GITHUB_ENV
 
-      - name: Set up Maven and Java
-        run: |
-          echo "Setting up Java and Maven"
-          ls -la /tools/java/
-          export JAVA_HOME=/tools/java/jdk17
-          export PATH=$JAVA_HOME/bin:$PATH
-          mvn -v
-          java -version
-          env | grep -e PATH -e JAVA_HOME
+      - name: Set up Java 17
+        uses: actions/setup-java@v3
+        with:
+          distribution: 'temurin' # Install Temurin (formerly AdoptOpenJDK)
+          java-version: '17'
 
       - name: Build the application
         run: |
