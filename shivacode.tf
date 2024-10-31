d8eb5d2c782b77462bec20a1a2bf4e2d44d4b1d6
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2024-11-01 02:06:22 +0530

    Update shivacode.tf

--- shivacode.tf
+++ shivacode.tf
@@ -1,45 +1,6 @@
-import { S3 } from 'aws-sdk';
-
-export const handler = async (event, context) => {
-  const s3 = new S3();
-  
-  const bucketName = 'www.enlume.com';
-  const objectKeyPrefix = 'www.enlume.com/media-images/';
-  
-  try {
-    // List all object versions in the specified S3 bucket and prefix
-    const objectVersions = await s3.listObjectVersions({
-      Bucket: bucketName,
-      Prefix: objectKeyPrefix,
-    }).promise();
-
-    // Extract older versions (excluding the latest) for deletion
-    const versionsToDelete = objectVersions.Versions.slice(1);
-
-    if (versionsToDelete.length > 0) {
-      // Create a list of objects to delete
-      const objectsToDelete = versionsToDelete.map(({ Key, VersionId }) => ({
-        Key,
-        VersionId,
-      }));
-
-      // Delete the older versions
-      await s3.deleteObjects({
-        Bucket: bucketName,
-        Delete: { Objects: objectsToDelete },
-      }).promise();
-    }
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify('Cache cleared successfully'),
-    };
-  } catch (error) {
-    console.error('Error clearing cache:', error);
-
-    return {
-      statusCode: 500,
-      body: JSON.stringify('Error clearing cache'),
-    };
-  }
-};
+- name: Azure Login
+  run: |
+    az login --service-principal \
+      --username "${{ secrets.AZURE_CLIENT_ID }}" \
+      --password "${{ secrets.AZURE_CLIENT_SECRET }}" \
+      --tenant "${{ secrets.AZURE_TENANT_ID }}"
