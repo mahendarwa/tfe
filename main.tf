62e8cbacae580781f640ba26398a8bb39907e8a9
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2024-11-14 01:50:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,3 @@
-- name: Set environment variables for Azure Key Vault access
-  run: |
-    echo "KEYVAULT_CLIENT_ID=${{ secrets.CRS_AZURE_CLIENT_ID_NONPROD }}" >> $GITHUB_ENV
-    echo "KEYVAULT_CLIENT_SECRET=${{ secrets.CRS_AZURE_CLIENT_SECRET_NONPROD }}" >> $GITHUB_ENV
-    echo "KEYVAULT_TENANT_ID=${{ secrets.CRS_AZURE_TENANT_ID_NONPROD }}" >> $GITHUB_ENV
+azure.keyvault.client-id=${KEYVAULT_CLIENT_ID}
+azure.keyvault.client-key=${KEYVAULT_CLIENT_SECRET}
+azure.keyvault.tenant-id=${KEYVAULT_TENANT_ID}
