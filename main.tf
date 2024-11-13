df81a52365ba9d5833c3208701db15a833552fc0
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2024-11-14 02:21:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+- name: Set up environment variables
+  run: |
+    echo "KEYVAULT_CLIENT_ID=${{ secrets.KEYVAULT_CLIENT_ID }}" >> $GITHUB_ENV
+    echo "KEYVAULT_CLIENT_KEY=${{ secrets.KEYVAULT_CLIENT_KEY }}" >> $GITHUB_ENV
+    echo "KEYVAULT_TENANT_ID=${{ secrets.KEYVAULT_TENANT_ID }}" >> $GITHUB_ENV
 azure.keyvault.client-id=${KEYVAULT_CLIENT_ID}
-azure.keyvault.client-key=${KEYVAULT_CLIENT_SECRET}
+azure.keyvault.client-key=${KEYVAULT_CLIENT_KEY}
 azure.keyvault.tenant-id=${KEYVAULT_TENANT_ID}
