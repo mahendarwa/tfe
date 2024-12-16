4c7fa748ad97bf1186126f6a1e5d163e996917bf
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2024-12-16 10:32:01 +0530

    Update shivacode.tf

--- shivacode.tf
+++ shivacode.tf
@@ -1,3 +1,6 @@
-Installing awscli and dbt-core alone pulls in 60 libraries as dependencies, Manually remove awscli and dbt-core dependencies one by one, and test if the application still works..
-OR
-Focus on identifying any other unused libraries outside of the above dependencies
+After discussing this with Craig, he suggested the following approach:
+
+Create a virtual environment to isolate the testing process.
+Take the existing (current) versions of the libraries and attempt to upgrade them to the fixed versions.
+Verify if the libraries function as expected without breaking any dependencies.
+I will proceed with this approach and provide updates based on my findings. Let me know if there are any additional checks or steps you’d like me to include.
