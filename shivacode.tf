9cb8e007e857fd1cf3d305712ebcdf6af0c231bc
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2024-12-16 10:13:43 +0530

    Update shivacode.tf

--- shivacode.tf
+++ shivacode.tf
@@ -1,11 +1,3 @@
-as part of the process to clean up and validate the libraries listed in requirement.txt, I followed the steps below:
-
-Created a virtual environment (Python version 3.9) on the Dev server cvlinq0177.
-Checked the existing libraries in the environment by executing the pip3 list command. I observed only two libraries:
-pip
-setuptools
-Installed the main required libraries (awscli and dbt-core) and rechecked the environment.
-After the installation, approximately 60 libraries were installed as dependencies (verified using pip3 list).
-The original requirement.txt file had 89 libraries. I compared the newly installed 60 libraries with the original 89, removed overlaps, and added the remaining libraries.
-This resulted in a total of 17 libraries (including dependencies), which are functionally equivalent to the original 89 libraries.
-To validate the setup, I ran the jobrunn.py script using the optimized set of 17 libraries, and it executed successfully.
+Installing awscli and dbt-core alone pulls in 60 libraries as dependencies, Manually remove awscli and dbt-core dependencies one by one, and test if the application still works..
+OR
+Focus on identifying any other unused libraries outside of the above dependencies
