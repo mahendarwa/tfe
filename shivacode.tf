3fc6541394acdcea6e1f0bdcb32fcd81f683787c
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2024-12-28 09:43:35 +0530

    Update shivacode.tf

--- shivacode.tf
+++ shivacode.tf
@@ -1,13 +1,19 @@
-- "*"                             # Wildcard host (Non-Compliant)
-- "/"                             # Root host without namespace
-- "other-namespace/service"       # Different namespace
-- "example.com"                   # diff domain 
-======
-metadata:
-  name: compliant-gateway
-  namespace: test-namespace
-spec:
-  servers:
-  - hosts:
-    - "test-namespace/app-service"   # Starts with namespace
-    - "test-namespace/web-service"   # Starts with namespace
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout Code
+        uses: actions/checkout@v3
+        with:
+          token: ${{ secrets.GITHUB_TOKEN }}
+
+      - name: Set up Git and Create Branch
+        run: |
+          git config --global user.name "GitHub Actions"
+          git config --global user.email "actions@github.com"
+          
+          DATE=$(date +%Y%m%d)
+          Branch="release-${DATE}-python"
+          
+          git checkout -b $Branch
+          git push origin $Branch
