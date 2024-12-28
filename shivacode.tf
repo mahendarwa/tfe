6da3a42a9961758fb5ff0972ff330fb940824aca
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2024-12-28 11:23:01 +0530

    Update shivacode.tf

--- shivacode.tf
+++ shivacode.tf
@@ -1,19 +1,40 @@
 jobs:
-  build:
-    runs-on: ubuntu-latest
+  deploy:
+    runs-on: zilverton-private-x64-ubuntu
+    permissions:
+      id-token: write
+      contents: read
+      packages: write
+    environment: ${{ github.event.inputs.ENVIRONMENT_TYPE }}
     steps:
       - name: Checkout Code
-        uses: actions/checkout@v3
+        uses: actions/checkout@v1
         with:
-          token: ${{ secrets.GITHUB_TOKEN }}
+          token: ${{ secrets.MY_GITHUB_TOKEN }}
 
-      - name: Set up Git and Create Branch
+      - name: Push to release
+        env:
+          GITHUB_TOKEN: ${{ secrets.MY_GITHUB_TOKEN }}
         run: |
-          git config --global user.name "GitHub Actions"
-          git config --global user.email "actions@github.com"
-          
+          git branch
           DATE=$(date +%Y%m%d)
           Branch="release-${DATE}-python"
-          
-          git checkout -b $Branch
-          git push origin $Branch
+
+          git config --global user.email "balaji.seetharman@cignahealthcare.com"
+          git config --global user.name "C8X6K9_Zilver"
+
+          # Create or switch to the branch
+          if git rev-parse --verify $Branch >/dev/null 2>&1; then
+            git checkout $Branch
+          else
+            git checkout -b $Branch
+          fi
+
+          # Add and commit changes if there are any
+          if [ -n "$(git status --porcelain)" ]; then
+            git add .
+            git commit -m "new release"
+            git push origin $Branch --force
+          else
+            echo "No changes to commit."
+          fi
