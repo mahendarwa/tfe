bc261d4f9e01cf990f868c0b0bd4157f05c530bc
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-02-18 18:42:33 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,6 +1,2 @@
-I am facing errors while executing the content processing script, due to naming discrepancies.
-
-For example:
-The script expects MV_Docs_Q12025, whereas the available directory is Model_Docs_Q12025/.
-Similarly, it looks for Updated_Negligible_Model_Risk_Memo.pdf, but the actual file name is Updated Negligible Model Risk Memo - Content Generation and Insights.pdf.
-Could you please review the paths and advise on the next steps?
+Python installed as system-wide package :	OS Team : ---	Might affect system tools, OS needs validation
+Python installed in application virtual environment: -- 	App Team:--  	No impact on OS, App team needs to validate
