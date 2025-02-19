a48c5f78a9b0811d25c54d0781edb59bd1e1af62
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-02-20 01:04:38 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,5 +1,6 @@
-sudo rm -rf /usr/local/lib/python3.6/site-packages/pip*
-sudo rm -rf /usr/local/bin/pip*
-curl https://bootstrap.pypa.io/pip/3.6/get-pip.py -o get-pip.py
-sudo /usr/local/bin/python3 get-pip.py
-/usr/local/bin/pip3 --version
+/usr/local/bin/python3 -m ensurepip --default-pip
+curl -O https://bootstrap.pypa.io/get-pip.py
+/usr/local/bin/python3 get-pip.py
+/usr/local/bin/python3 -m pip --version
+/usr/local/bin/python3 -m pip install sqlparse
+/usr/local/bin/python3 -c "import pip; print(pip.__file__)"
