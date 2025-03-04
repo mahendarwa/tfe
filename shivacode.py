18f20e8e36c644b91df26dc33f7f0838a6a55f63
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-03-05 01:28:42 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,4 +1,14 @@
-Add-WindowsFeature -Name OpenSSH-Server
-Start-Service sshd
-Set-Service -Name sshd -StartupType Automatic
-netsh advfirewall firewall add rule name="OpenSSH" dir=in action=allow protocol=TCP localport=22
+package aks_kspm_8
+
+default result = "pass"
+
+result = "fail" {
+    some container
+    input.metadata.annotations[sprintf("container.apparmor.security.beta.kubernetes.io/%s", [container])] != "runtime/default"
+}
+
+
+currentConfiguration := sprintf("At least one container has AppArmor profile set to '%s'", [input.metadata.annotations[sprintf("container.apparmor.security.beta.kubernetes.io/%s", [container])]])
+
+
+expectedConfiguration := "All containers should have AppArmor profile set to 'runtime/default'"
