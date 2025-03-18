05e1aef7c0e18317571ce0743531f7c89a7b198b
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-03-18 23:05:02 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -2,11 +2,9 @@ import pyodbc
 
 print("Connecting to SQL Server...")
 
-# Hardcoded username & password
 username = "Internal\\c8x6k9"
 password = "Dev2s@mpath"
 
-# Correct driver name based on your logs
 connection_string = (
     "DRIVER={ODBC Driver 17 for SQL Server};"
     "SERVER=HSTNCMSRDIDEV01.healthspring.inside;"
@@ -20,9 +18,9 @@ try:
     cursor = conn.cursor()
     cursor.execute("SELECT @@VERSION;")
     row = cursor.fetchone()
-    print("✅ Connection successful!")
+    print("Connection successful!")
     print(f"SQL Server version: {row[0]}")
     cursor.close()
     conn.close()
 except pyodbc.Error as e:
-    print("❌ Connection failed:", e)
+    print("Connection failed:", e)
