7166a75c3ccebc3fcdd80324e5d074b92609aecb
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-04-01 01:53:19 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,17 +1,3 @@
-stage('Start service') {
-    agent { label 'consumer-panel-services-agent_prod_daylnxcpsp014' }
-
-    steps {
-        script {
-            sh """
-                ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode pfnzsup3@${env.server}.enterprisenet.org '
-                    echo Connected to \$(hostname) as \$(whoami)
-                    cd /pfnz/bin
-                    chmod +x startPATservice_${params.prod_envi}.sh
-                    nohup ./startPATservice_${params.prod_envi}.sh > service.log 2>&1 &
-                    echo 🚀 Service start triggered
-                '
-            """
-        }
-    }
-}
+query_type = sys.argv[1] if len(sys.argv) > 1 else "list_tables"
+server = sys.argv[2] if len(sys.argv) > 2 else "HSTNCMSRDIQA02.healthspring.inside"
+database = sys.argv[3] if len(sys.argv) > 3 else "Staging"
