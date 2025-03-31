a99c6597e99788361e7e9980048dbad3c70a9ade
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-03-31 23:39:31 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -5,22 +5,21 @@ stage('Start service') {
     script {
       def remoteScriptContent = """
         #!/bin/bash
-        nohup ~/startPATservice_${params.prod_envi}.sh > service.log 2>&1 &
+        nohup /pfnz/home/pfnzsup3/startPATservice_${params.prod_envi}.sh > /pfnz/home/pfnzsup3/service.log 2>&1 &
       """
 
-      // Save script to workspace
       writeFile file: 'remote_start_service.sh', text: remoteScriptContent
 
-      // Remove previous files and copy new scripts + jar using working ssh key
-      sh "ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode pfmgrap@${env.server}.enterprisenet.org 'rm -rf ~/caseservice-PAT.jar'"
-      sh "ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode pfmgrap@${env.server}.enterprisenet.org 'cp /${params.prod_envi}/bin/caseservice-PAT.jar ~/'"
+      sh "ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode pfmgrap@${env.server}.enterprisenet.org 'rm -f /pfnz/home/pfnzsup3/caseservice-PAT.jar'"
+      sh "ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode pfmgrap@${env.server}.enterprisenet.org 'cp /${params.prod_envi}/bin/caseservice-PAT.jar /pfnz/home/pfnzsup3/'"
 
-      sh "ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode pfmgrap@${env.server}.enterprisenet.org 'rm -rf ~/startPATservice_${params.prod_envi}.sh'"
-      sh "ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode pfmgrap@${env.server}.enterprisenet.org 'cp /${params.prod_envi}/bin/startPATservice_${params.prod_envi}.sh ~/'"
+      sh "ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode pfmgrap@${env.server}.enterprisenet.org 'rm -f /pfnz/home/pfnzsup3/startPATservice_${params.prod_envi}.sh'"
+      sh "ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode pfmgrap@${env.server}.enterprisenet.org 'cp /${params.prod_envi}/bin/startPATservice_${params.prod_envi}.sh /pfnz/home/pfnzsup3/'"
 
-      // Copy and run script remotely
-      sh "scp -o StrictHostKeyChecking=no -i ~/.ssh/vscode remote_start_service.sh pfmgrap@${env.server}.enterprisenet.org:~/"
-      sh "ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode pfmgrap@${env.server}.enterprisenet.org 'chmod +x ~/remote_start_service.sh && bash ~/remote_start_service.sh'"
+      sh "scp -o StrictHostKeyChecking=no -i ~/.ssh/vscode remote_start_service.sh pfmgrap@${env.server}.enterprisenet.org:/pfnz/home/pfnzsup3/"
+      sh "ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode pfmgrap@${env.server}.enterprisenet.org 'chmod +x /pfnz/home/pfnzsup3/remote_start_service.sh'"
+
+      sh "ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode pfnzsup3@${env.server}.enterprisenet.org 'bash ~/remote_start_service.sh'"
     }
   }
 }
