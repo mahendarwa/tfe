e274d90a134357cc73dfb92eb165a53304ed1dd2
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-04-01 01:57:11 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,3 +1,2 @@
-query_type = sys.argv[1] if len(sys.argv) > 1 else "list_tables"
-server = sys.argv[2] if len(sys.argv) > 2 else "HSTNCMSRDIQA02.healthspring.inside"
-database = sys.argv[3] if len(sys.argv) > 3 else "Staging"
+rf'SERVER={server};'
+rf'DATABASE={database};'
