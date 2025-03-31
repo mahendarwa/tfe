c1da0ca29ba35bbb8af9751fc0fb2a991c78290a
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-03-31 22:24:38 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,15 +1,8 @@
-$sharePath = "\\HSTNTDM01.Healthspring.Inside"
-
-Write-Output "Checking access to $sharePath..."
-Get-ChildItem -Path $sharePath
-
-
-$sharePath = "\\HSTNTDM01.Healthspring.Inside"
-
-Write-Output "Listing contents of $sharePath..."
-[System.IO.Directory]::GetDirectories($sharePath)
-
-
-
-$sharePath = "\\HSTNTDM01.Healthspring.Inside\TDM_SQL2TD"
-Get-ChildItem -Path $sharePath
+sudo yum groupinstall "Development Tools" -y
+sudo yum install gcc openssl-devel bzip2-devel libffi-devel wget -y
+cd /usr/src
+sudo wget https://www.python.org/ftp/python/3.11.0/Python-3.11.0.tgz
+sudo tar xzf Python-3.11.0.tgz
+cd Python-3.11.0
+sudo ./configure --enable-optimizations
+sudo make altinstall
