029d88d3ac0bf624dcdac362e9cf4b6e01475a85
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-03-31 23:11:44 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,8 +1,30 @@
-sudo yum groupinstall "Development Tools" -y
-sudo yum install gcc openssl-devel bzip2-devel libffi-devel wget -y
-cd /usr/src
-sudo wget https://www.python.org/ftp/python/3.11.0/Python-3.11.0.tgz
-sudo tar xzf Python-3.11.0.tgz
-cd Python-3.11.0
-sudo ./configure --enable-optimizations
-sudo make altinstall
+stage('Kill service') {
+  agent { label 'consumer-panel-services-agent_prod_daylnxcpsp014' }
+
+  steps {
+    script {
+      def sshCommand = """
+        ssh -o StrictHostKeyChecking=no ${env.adminuser}@${env.server}.enterprisenet.org '
+          echo " Checking for PID on port ${env.server_port} for user ${env.adminuser}..."
+
+          pid=\$(ps -u ${env.adminuser} -o pid= | xargs -I {} sh -c "ps -o args= -p {} | grep -q \\"Dserver.port=${env.server_port}\\" && echo {}")
+          
+          if [ -n "\$pid" ]; then
+            echo "Found PID: \$pid. Attempting to kill..."
+            kill -9 \$pid
+            kill_status=\$?
+            if [ \$kill_status -eq 0 ]; then
+              echo "Process with PID \$pid killed successfully."
+            else
+              echo "Failed to kill process with PID \$pid. Exit code: \$kill_status"
+              exit 1
+            fi
+          else
+            echo "  No matching process found for user ${env.adminuser} on port ${env.server_port}."
+          fi
+        '
+      """
+      sh returnStatus: true, script: sshCommand
+    }
+  }
+}
