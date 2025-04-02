ca4d26bce26d601f977834958b907e67e526ce62
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-04-02 13:56:11 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,39 +1,33 @@
-import os
-import sys
-import pyodbc
+package wiz
 
-connection = None
-cursor = None
+default result = "pass"
 
-query_type = sys.argv[1] if len(sys.argv) > 1 else "list_tables"
-server = sys.argv[2] if len(sys.argv) > 2 else "HSTNCMSRDIQA02.healthspring.inside"
-database = sys.argv[3] if len(sys.argv) > 3 else "Staging"
+# Fail if pod-level is false and container-level is undefined
+result = "fail" {
+    input.spec.securityContext.runAsNonRoot == false
+    not input.spec.containers[_].securityContext.runAsNonRoot
+}
 
-uid = os.getenv("SQL_UID")
-pwd = os.getenv("SQL_PWD")
+# Fail if pod-level is false and container-level is false
+result = "fail" {
+    input.spec.securityContext.runAsNonRoot == false
+    input.spec.containers[_].securityContext.runAsNonRoot == false
+}
 
-connection_string = (
-    r'DRIVER={ODBC Driver 18 for SQL Server};'
-    rf'SERVER={server};'
-    rf'DATABASE={database};'
-    r'Trusted_Connection=yes;'
-    r'DOMAIN=INTERNAL;'
-    rf'UID={uid};'
-    rf'PWD={pwd};'
-    r'TrustServerCertificate=yes;'
-)
+# Fail if pod-level is true and container-level is false
+result = "fail" {
+    input.spec.securityContext.runAsNonRoot == true
+    input.spec.containers[_].securityContext.runAsNonRoot == false
+}
 
-connection = pyodbc.connect(connection_string)
-cursor = connection.cursor()
-print(f"Connected to SQL Server:--  {server}")
+# Fail if pod-level is not defined and container-level is false
+result = "fail" {
+    not input.spec.securityContext.runAsNonRoot
+    input.spec.containers[_].securityContext.runAsNonRoot == false
+}
 
-if query_type == "list_tables":
-    with open("sql_cmd.sql", "r") as file:
-        sql_query = file.read()
-    cursor.execute(sql_query)
-    for row in cursor.fetchall():
-        print(f"Table: {row.TABLE_SCHEMA}.{row.TABLE_NAME}")
-
-cursor.close()
-connection.close()
-print("Connection closed")
+# Fail if pod-level is not defined and container-level is also not defined
+result = "fail" {
+    not input.spec.securityContext.runAsNonRoot
+    not input.spec.containers[_].securityContext.runAsNonRoot
+}
