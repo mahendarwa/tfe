d1313aa55e7b798b021340b3cfedecb908047bee
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-04-22 02:42:01 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,17 +1,1 @@
-# Create a working directory
-mkdir -p ~/teradata-ttu && cd ~/teradata-ttu
-
-# Download the required RPMs
-wget https://downloads.teradata.com/download/cdn/ttu/17.20/linux/teradata-client/teradata-libs-17.20.15.00-1.noarch.rpm
-wget https://downloads.teradata.com/download/cdn/ttu/17.20/linux/teradata-client/tdicu-17.20.15.00-1.noarch.rpm
-wget https://downloads.teradata.com/download/cdn/ttu/17.20/linux/teradata-client/cliv2-17.20.15.00-1.noarch.rpm
-wget https://downloads.teradata.com/download/cdn/ttu/17.20/linux/teradata-client/bteq-17.20.15.00-1.noarch.rpm
-
-# Verify files
-ls -lh
-
-# Install all downloaded RPMs
-sudo dnf install ./*.rpm -y
-
-# Test bteq
-bteq
+sudo bash -c 'echo "<IP> HSTNTDDEV.healthspring.inside" >> /etc/hosts'
