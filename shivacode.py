e9823d246782b1828d1c846375a72bd0769f247e
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-04-23 22:31:11 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,5 +1,1 @@
-sudo su
-curl https://packages.microsoft.com/config/rhel/7/prod.repo > /etc/yum.repos.d/msprod.repo
-yum install -y msodbcsql17 unixODBC
-
-yum install -y mssql-tools
+export PATH="$PATH:/opt/mssql-tools/bin"
