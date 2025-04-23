388fef700af02a2434e8e8a01595479c5d8af044
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-04-23 22:13:31 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,1 +1,4 @@
-scp -r /tmp/tt/teradata pythonprod@hdclappxp064:/tmp/
+export TDDIR=/opt/teradata/client/17.10
+export TDICU=$TDDIR/tdicu
+export LD_LIBRARY_PATH=$TDDIR/lib:$TDICU/lib:$LD_LIBRARY_PATH
+export PATH=$TDDIR/bin:$PATH
