43eae7db4c4004e61f008dff4ad5bec1bb170d31
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-04-25 00:05:58 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -2,26 +2,28 @@ package gcp.firewall
 
 default result = "pass"
 
-deny[reason] {
+deny_reason[reason] {
   input.kind == "compute#firewall"
   input.direction == "EGRESS"
   not input.disabled
   input.destinationRanges[_] == "0.0.0.0/0"
+
   some i
-  input.allowed[i].IPProtocol == "all"  # Completely unrestricted
+  input.allowed[i].IPProtocol == "all"
   reason := "Firewall rule allows all protocols to 0.0.0.0/0"
 }
 
-deny[reason] {
+deny_reason[reason] {
   input.kind == "compute#firewall"
   input.direction == "EGRESS"
   not input.disabled
   input.destinationRanges[_] == "0.0.0.0/0"
+
   some i
-  input.allowed[i].ports[_] == "0-65535"  # Fully open port range
+  input.allowed[i].ports[_] == "0-65535"
   reason := "Firewall rule allows full port range to 0.0.0.0/0"
 }
 
 result := "fail" {
-  deny[_]
+  deny_reason[_]
 }
