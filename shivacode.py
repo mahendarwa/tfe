da14517572af9ab2bc38cb9f122d2d64c4d12c0e
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-05-01 23:39:38 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,19 +1,3 @@
- - name: Set Feature Branch Name
-        run: |
-          BASE_NAME="feature-${{ github.event.inputs.ReleaseYear }}-${{ github.event.inputs.ReleaseMonth }}-${{ github.event.inputs.ReleaseDate }}"
-          if [ -n "${{ github.event.inputs.ReleaseReference }}" ]; then
-            FEATURE_BRANCH="${BASE_NAME}-${{ github.event.inputs.ReleaseReference }}"
-          else
-            FEATURE_BRANCH="${BASE_NAME}"
-          fi
-          echo "FEATURE_BRANCH=${FEATURE_BRANCH}" >> $GITHUB_ENV
-
-      - name: Create and Push Feature Branch
-        run: |
-          git config --global user.name "c8x6k9"
-          git config --global user.email "C8X6K9_Zilver@github.com"
-          git remote -v
-          git ls-remote origin
-          git checkout -b $FEATURE_BRANCH
-          git remote set-url origin https://x-access-token:${{ secrets.MY_GITHUB_TOKEN }}@github.com/zilvertonz/GBS_DAE_Python_ETL.git
-          git push origin $FEATURE_BRANCH
+git fetch origin
+git merge origin/develop --strategy=ours --no-edit --allow-unrelated-histories
+git push https://x-access-token:${{ secrets.MY_GITHUB_TOKEN }}@github.com/zilvertonz/GBS_DAE_Python_ETL.git HEAD:${Branch} --force
