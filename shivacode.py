02e93aec3e1c28e8849a6d7c599ba57d260e2a07
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-05-01 23:09:17 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,1 +1,6 @@
-git remote set-url origin https://x-access-token:${{ secrets.MY_GITHUB_TOKEN }}@github.com/zilvertonz/GBS_DAE_Python_ETL.git
+- name: Verify Git user and remote
+  run: |
+    git config --global user.name "GitHub Actions"
+    git config --global user.email "actions@github.com"
+    git remote -v
+    git ls-remote origin
