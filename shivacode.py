f61b8dd220b92a97ea045c8043e81821ab74bfd5
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-05-02 16:52:10 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,1 +1,1 @@
-sudo su -c 'curl -s https://packages.microsoft.com/config/rhel/7/prod.repo > /etc/yum.repos.d/msprod.repo && ACCEPT_EULA=Y yum install -y msodbcsql17'
+sudo su -c 'curl -s https://packages.microsoft.com/config/rhel/8/prod.repo > /etc/yum.repos.d/msprod.repo && ACCEPT_EULA=Y yum install -y msodbcsql17'
