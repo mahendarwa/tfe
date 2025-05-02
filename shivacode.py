2761c4bddc34d87e42fbb408c398ca13c85e7ed3
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-05-02 16:54:48 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,1 +1,4 @@
-sudo su -c 'curl -s https://packages.microsoft.com/config/rhel/8/prod.repo > /etc/yum.repos.d/msprod.repo && ACCEPT_EULA=Y yum install -y msodbcsql17'
+sudo yum install -y curl unixODBC unixODBC-devel
+curl -O https://packages.microsoft.com/rhel/8/prod/msodbcsql17-17.10.6.1-1.x86_64.rpm
+sudo ACCEPT_EULA=Y yum install -y msodbcsql17-17.10.6.1-1.x86_64.rpm
+odbcinst -q -d | grep "ODBC Driver 17 for SQL Server"
