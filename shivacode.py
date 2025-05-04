376ecc44c6f2f263b2e3bf9d716c7ad39c7451f4
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-05-04 23:47:37 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -5,6 +5,6 @@
     git config --global user.name "C8X6K9_Zilver"
     git fetch origin develop
     git fetch origin ${Branch}
-    git checkout -b develop origin/develop
-    git merge origin/${Branch} --strategy=ours --no-edit --allow-unrelated-histories
-    git push https://x-access-token:${{ secrets.MY_GITHUB_TOKEN }}@github.com/zilvertonz/GBS_DAE_Python_ETL.git develop --force
+    git checkout -b ${Branch} origin/${Branch}
+    git merge origin/develop --strategy=ours --no-edit --allow-unrelated-histories
+    git push https://x-access-token:${{ secrets.MY_GITHUB_TOKEN }}@github.com/zilvertonz/GBS_DAE_Python_ETL.git HEAD:develop --force
