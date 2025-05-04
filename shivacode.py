a5bab8f8e9549b3dc2313c50e74839ec03f44170
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-05-05 00:06:19 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,15 +1,10 @@
-Branch=${{ github.event.inputs.feature_branch }}
-
-git config --global user.email "balaji.seetharaman@cignahealthcare.com"
-git config --global user.name "C8X6K9_Zilver"
-
-git remote set-url origin https://x-access-token:${{ secrets.MY_GITHUB_TOKEN }}@github.com/zilvertonz/GBS_DAE_Python_ETL.git
-git fetch origin
-
-git checkout develop || git checkout -b develop origin/develop
-git pull origin develop
-
-git fetch origin $Branch:$Branch || echo "Feature branch $Branch already exists locally"
-git merge $Branch --strategy=ours --no-edit --allow-unrelated-histories || true
-
-git push origin develop --force
+- name: Merge Feature to Develop Branch
+  run: |
+    Branch=${{ github.event.inputs.feature_branch }}
+    git config --global user.email "balaji.seetharaman@cignahealthcare.com"
+    git config --global user.name "C8X6K9_Zilver"
+    git fetch origin develop
+    git fetch origin $Branch
+    git checkout develop || git checkout -t origin/develop
+    git merge origin/$Branch --strategy=ours --no-edit --allow-unrelated-histories
+    git push https://${{ secrets.MY_GITHUB_TOKEN }}@github.com/zilvertonz/GBS_DAE_Python_ETL.git develop --force
