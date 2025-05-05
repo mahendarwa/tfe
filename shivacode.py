de9259e690c8970bbd23f377353c335e6810b390
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-05-05 22:14:02 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,16 +1,16 @@
 - name: Merge Feature to Develop Branch
   run: |
     Branch=${{ github.event.inputs.feature_branch }}
+
     git config --global user.email "balaji.seetharaman@cignahealthcare.com"
     git config --global user.name "C8X6K9_Zilver"
 
-    git fetch origin
+    git fetch --all
     git checkout develop
     git reset --hard origin/develop
 
     git merge origin/$Branch --strategy=ours --no-edit --allow-unrelated-histories || true
-
-    git checkout $Branch -- .
+    git checkout origin/$Branch -- .
     git add .
     git commit -m "Merge feature branch ($Branch) into develop, preferring feature changes"
     git push https://x-access-token:${{ secrets.MY_GITHUB_TOKEN }}@github.com/zilvertonz/GBS_DAE_Python_ETL.git develop --force
