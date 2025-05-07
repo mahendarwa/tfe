7c3236025656621a5070919bd28c9cd6b8bf1e17
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-05-07 23:31:41 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,15 +1,14 @@
+#servers
 
+[DEV]
+HDCLAPPXD135.Healthspring.inside ansible_user=pythondev ansible_ssh_pass="Pydev426#" ansible_ssh_extra_args="-o StrictHostKeyChecking=no -o HostKeyAlgorithms=+sk-ssh-ed25519@openssh.com"
+[QA]
+HDCLAPPXD133.Healthspring.inside ansible_user=pythonqa ansible_ssh_pass="Pytqa427#" ansible_ssh_extra_args="-o StrictHostKeyChecking=no -o HostKeyAlgorithms=+sk-ssh-ed25519@openssh.com"
+[INT]
+HDCLAPPXD134.Healthspring.inside ansible_user=pythonint ansible_ssh_pass="Pyint425#" ansible_ssh_extra_args="-o StrictHostKeyChecking=no -o HostKeyAlgorithms=+sk-ssh-ed25519@openssh.com"
+[UAT]
+HDCLAPPXD132.Healthspring.inside ansible_user=pythonuat ansible_ssh_pass="Pyuat425#" ansible_ssh_extra_args="-o StrictHostKeyChecking=no -o HostKeyAlgorithms=+sk-ssh-ed25519@openssh.com"
+[UAT2]
 
-./svc.sh stop || true
+[PROD]
 
-./config.sh remove --unattended --token <OLD_RUNNER_TOKEN>
-
-
-./config.sh --unattended \
-  --url https://github.com/<your_org_or_repo_path> \
-  --token <NEW_RUNNER_TOKEN> \
-  --name daylnxcpsq014 \
-  --labels self-hosted,Linux,X64,prod14-runner
-
-./svc.sh install
-./svc.sh start
