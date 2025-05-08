372b22e1c2725b19a6a70b89e39a9fb7d92f5467
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-05-08 23:46:56 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,14 +1,1 @@
-#servers
-
-[DEV]
-HDCLAPPXD135.Healthspring.inside ansible_user=pythondev ansible_ssh_pass="Pydev426#" ansible_ssh_extra_args="-o StrictHostKeyChecking=no -o HostKeyAlgorithms=+sk-ssh-ed25519@openssh.com"
-[QA]
-HDCLAPPXD133.Healthspring.inside ansible_user=pythonqa ansible_ssh_pass="Pytqa427#" ansible_ssh_extra_args="-o StrictHostKeyChecking=no -o HostKeyAlgorithms=+sk-ssh-ed25519@openssh.com"
-[INT]
-HDCLAPPXD134.Healthspring.inside ansible_user=pythonint ansible_ssh_pass="Pyint425#" ansible_ssh_extra_args="-o StrictHostKeyChecking=no -o HostKeyAlgorithms=+sk-ssh-ed25519@openssh.com"
-[UAT]
-HDCLAPPXD132.Healthspring.inside ansible_user=pythonuat ansible_ssh_pass="Pyuat425#" ansible_ssh_extra_args="-o StrictHostKeyChecking=no -o HostKeyAlgorithms=+sk-ssh-ed25519@openssh.com"
-[UAT2]
-
-[PROD]
-
+cigna_dna_gbsa_oss_etl_library>=0.2.0,<1.0.0
