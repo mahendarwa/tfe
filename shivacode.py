a02711f43ee117724810c085be9068e75e19d03e
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-05-13 01:15:30 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,19 +1,5 @@
-package wiz
-
-default result := "pass"
-
 result := "fail" {
 	input.properties.type == "artifactregistry#repository"
-	input.properties.hasIAMAccessFromOutsideOrganization
-	not input.properties.hasIAMAccessFromExternalSubscription
-}
-
-currentConfiguration := {
-	"hasIAMAccessFromOutsideOrganization": input.properties.hasIAMAccessFromOutsideOrganization,
-	"hasIAMAccessFromExternalSubscription": input.properties.hasIAMAccessFromExternalSubscription
-}
-
-expectedConfiguration := {
-	"hasIAMAccessFromOutsideOrganization": false,
-	"hasIAMAccessFromExternalSubscription": true
+	count(input.properties.hasIAMAccessFromOutsideOrganization) > 0
+	count(input.properties.hasIAMAccessFromExternalSubscription) == 0
 }
