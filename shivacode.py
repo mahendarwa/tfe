f238c03e3202b8054b735cf485118a3930582d29
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-05-12 23:00:41 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,1 +1,11 @@
-cigna_dna_gbsa_oss_etl_library>=0.2.0,<1.0.0
+package wiz
+
+default result := "pass"
+
+result := "fail" {
+  input["kind"] == "compute#natGateway"
+  input["type"] != "PRIVATE"
+}
+
+currentConfiguration := sprintf("kind: %v, type: %v", [input.kind, input.type])
+expectedConfiguration := "kind: compute#natGateway, type: PRIVATE"
