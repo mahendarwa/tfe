8aaea2254540bfdac2075b54ef27c50f74654789
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-05-29 23:05:23 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,7 +1,4 @@
 result := "fail" {
   input.properties.nativeType == "dataflow#job"
-  not input.properties.region == allowed_types[_]
+  not input.properties.region in allowed_types
 }
-
-currentConfiguration := sprintf("Region used: %v", [input.properties.region])
-expectedConfiguration := sprintf("Allowed regions: %v", [allowed_types])
