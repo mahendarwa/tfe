5dd40b45955be28e77d9465699afa71abda6bc63
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-05-29 23:02:51 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,27 +1,7 @@
-- name: Create teradata module
-  run: |
-    set -e
-    set -x
+result := "fail" {
+  input.properties.nativeType == "dataflow#job"
+  not input.properties.region == allowed_types[_]
+}
 
-    cd GBS_DAE_OSS
-    git fetch origin
-
-    if git ls-remote --exit-code --heads origin "$feature_branch"; then
-      git checkout "$feature_branch"
-
-      git config pull.rebase false
-      git config pull.ff false
-
-
-      git pull origin "$feature_branch" --allow-unrelated-histories || {
-        echo "⚠️ Merge conflict detected. Auto-resolving using remote branch..."
-        git checkout --theirs .
-        git add .
-        git commit -m "🔧 Auto-resolved merge conflicts using remote branch (theirs)"
-      }
-    else
-      echo "❌ Branch $feature_branch not found in remote."
-      exit 1
-    fi
-
-    ls -l
+currentConfiguration := sprintf("Region used: %v", [input.properties.region])
+expectedConfiguration := sprintf("Allowed regions: %v", [allowed_types])
