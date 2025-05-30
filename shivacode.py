c3233996e7480371d250abe81272a08becc8d5c3
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-05-30 20:39:10 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,4 +1,8 @@
-result := "fail" {
-  input.properties.nativeType == "dataflow#job"
-  not input.properties.region in allowed_types
-}
+- name: Create GitHub Release using CLI
+  run: |
+    cd GBS_DAE_OSS
+    echo "Build version: ${{ env.FEATURE_BUILD_VERSION }}"
+
+    gh release create "${{ env.FEATURE_BUILD_VERSION }}" ../odms-teradata-release.tgz \
+      --title "Release ${{ env.FEATURE_BUILD_VERSION }}" \
+      --notes "Automated Teradata build for ${{ github.event.inputs.feature_branch }}. Build version: ${{ env.FEATURE_BUILD_VERSION }}"
