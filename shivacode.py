786ca3a4a20f769fb035394aa8531966ca258acc
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-05-30 20:45:43 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,8 +1,7 @@
 - name: Create GitHub Release using CLI
   run: |
-    cd GBS_DAE_OSS
     echo "Build version: ${{ env.FEATURE_BUILD_VERSION }}"
 
-    gh release create "${{ env.FEATURE_BUILD_VERSION }}" ../odms-teradata-release.tgz \
+    gh release create "${{ env.FEATURE_BUILD_VERSION }}" ./odms-teradata-release.tgz \
       --title "Release ${{ env.FEATURE_BUILD_VERSION }}" \
       --notes "Automated Teradata build for ${{ github.event.inputs.feature_branch }}. Build version: ${{ env.FEATURE_BUILD_VERSION }}"
