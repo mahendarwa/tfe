e6082c34b8d2802c7b8754624976d7a0601ac3a2
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-06-11 11:57:59 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,2 +1,6 @@
-const versionForUrl = versionString.replaceAll('.', '-');
-const swaggerUrl = `${baseUrls[env]}-${versionForUrl}${domains[env]}/ae/swagger-ui/index.html`;
+- name: Set ENV_ID from input
+  run: echo "ENV_ID=${{ github.event.inputs.environment }}" >> $GITHUB_ENV
+
+
+
+executionenv = os.getenv("ENV_ID", "")
