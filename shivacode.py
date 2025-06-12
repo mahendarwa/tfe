c32a1ad0770270b633153ccff0981e73a8c3836c
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-06-13 00:16:56 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,1 +1,7 @@
-.logon HSTNTDPROD.HealthSpring.Inside/AUTOCHG_USER_CHS_PRD, $_bdgE7r1#Tr
+bteq <<EOF
+.logon HSTNTDPROD.HealthSpring.Inside/AUTOCHG_USER_CHS_PRD
+$_bdgE7r1#Tr
+.run file=final_script.sql;
+.logoff;
+.quit;
+EOF
