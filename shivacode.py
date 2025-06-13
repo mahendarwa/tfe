98bbaad0402f6d75219a265ea6a9a01e1458796f
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-06-13 21:35:33 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,1 +1,28 @@
-pwd_escaped = pwd.replace('$', '\\$')
+# Set bteq_cmd based on execution environment
+if executionenv.upper() == "UAT":
+    bteq_cmd = f"""
+bteq <<EOF
+.logon {host}/{user},RpSQC\\$c_4dwv;
+.run file=final_script.sql;
+.logoff;
+.quit;
+EOF
+"""
+elif executionenv.upper() == "PRD":
+    bteq_cmd = f"""
+bteq <<EOF
+.logon {host}/{user},T#4!@asd*&;
+.run file=final_script.sql;
+.logoff;
+.quit;
+EOF
+"""
+else:
+    bteq_cmd = f"""
+bteq <<EOF
+.logon {host}/{user},{pwd};
+.run file=final_script.sql;
+.logoff;
+.quit;
+EOF
+"""
