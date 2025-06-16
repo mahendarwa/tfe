cf3981bda7ed4c150e983b7d475c0bbad66c3d46
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-06-16 21:59:14 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,28 +1,10 @@
-# Set bteq_cmd based on execution environment
-if executionenv.upper() == "UAT":
-    bteq_cmd = f"""
-bteq <<EOF
-.logon {host}/{user},RpSQC\\$c_4dwv;
-.run file=final_script.sql;
-.logoff;
-.quit;
-EOF
-"""
-elif executionenv.upper() == "PRD":
-    bteq_cmd = f"""
-bteq <<EOF
-.logon {host}/{user},T#4!@asd*&;
-.run file=final_script.sql;
-.logoff;
-.quit;
-EOF
-"""
-else:
-    bteq_cmd = f"""
-bteq <<EOF
-.logon {host}/{user},{pwd};
-.run file=final_script.sql;
-.logoff;
-.quit;
-EOF
-"""
+package wiz
+
+default result := "pass"
+
+result := "fail" {
+  not input.controlPlaneEndpointsConfig.ipEndpointsConfig.authorizedNetworksConfig.privateEndpointEnforcementEnabled
+}
+
+currentConfiguration := sprintf("privateEndpointEnforcementEnabled: %v", [input.controlPlaneEndpointsConfig.ipEndpointsConfig.authorizedNetworksConfig.privateEndpointEnforcementEnabled])
+expectedConfiguration := "privateEndpointEnforcementEnabled: true"
