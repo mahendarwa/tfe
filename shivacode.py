63f9a06e1da5f6421ad7d5315602f6af979830ba
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-06-24 01:32:17 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -10,4 +10,23 @@ result = "fail" {
 currentConfiguration := sprintf("LB annotation: '%v'", [input.metadata.annotations["service.beta.kubernetes.io/azure-load-balancer-internal"]])
 expectedConfiguration := "LoadBalancer must be internal (azure-load-balancer-internal: true)"
 
+========
+
+
+package wiz
+
+default result = "pass"
+
+
+result = "fail" {
+    input.spec.type == "LoadBalancer"
+    not input.metadata.annotations["networking.gke.io/load-balancer-type"] == "Internal"
+}
+
+currentConfiguration := sprintf(
+    "LoadBalancer annotation: '%v'", 
+    [input.metadata.annotations["networking.gke.io/load-balancer-type"]]
+)
+
+expectedConfiguration := "Service of type LoadBalancer must have annotation networking.gke.io/load-balancer-type: 'Internal'"
 
