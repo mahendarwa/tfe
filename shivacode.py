891ecbb3141000376cac591fa8483710141b3188
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-06-25 00:53:17 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -31,11 +31,9 @@ jobs:
           echo "❌ Unauthorized actor: ${{ github.actor }}"
           exit 1
 
-  prepare-group:
+  PAT:
     needs: validate-actor
     runs-on: prod-runner
-    outputs:
-      envs: ${{ steps.select_group.outputs.envs }}
     steps:
       - name: Select environments for group
         id: select_group
@@ -50,122 +48,106 @@ jobs:
             echo "Invalid group selection"
             exit 1
           fi
-          echo "Selected group: ${{ github.event.inputs.Group }}"
-          echo "Environments: ${GROUP_ENVIRONMENTS[@]}"
-          echo "envs=$(printf '%s\n' "${GROUP_ENVIRONMENTS[@]}" | jq -R . | jq -cs .)" >> $GITHUB_OUTPUT
 
-  PAT:
-    needs: prepare-group
-    runs-on: prod-runner
-    strategy:
-      matrix:
-        environment: ${{ fromJson(needs.prepare-group.outputs.envs) }}
-    steps:
+          echo "GROUP_ENVIRONMENTS=${GROUP_ENVIRONMENTS[@]}" >> $GITHUB_ENV
+
       - name: Checkout repo
         uses: actions/checkout@v3
 
-      - name: Set Admin User and Path
-        id: set_vars
+      - name: Run PAT steps for each environment
         run: |
-          ENV="${{ matrix.environment }}"
-          case "$ENV" in
-            pfnz|pfid|pfph|pfth|pfsg|pfhk|pfau|pfbp|omau)
-              echo "ADMIN_USR=pfmgrap" >> $GITHUB_ENV
-              ;;
-            pfca|omca|pfcl|pfco|pfpr|pfbr|pfmx|pfxu|pfus)
-              echo "ADMIN_USR=pfmgram" >> $GITHUB_ENV
-              ;;
-            pffi|pfra|pfgb|pfpt|pfes|pffr|pfde|pfch|pfit)
-              echo "ADMIN_USR=pfmgreu" >> $GITHUB_ENV
-              ;;
-            *)
-              echo "Invalid environment: $ENV"
-              exit 1
-              ;;
-          esac
+          for ENV in ${GROUP_ENVIRONMENTS}; do
+            echo "==============================="
+            echo "Processing environment: $ENV"
+            echo "==============================="
 
-          # Set PATH_ENV
-          case "$ENV" in
-            pfnz) echo "PATH_ENV=AP_Region/NZ/" >> $GITHUB_ENV ;;
-            pfid) echo "PATH_ENV=AP_Region/ID/" >> $GITHUB_ENV ;;
-            pfph) echo "PATH_ENV=AP_Region/PH/" >> $GITHUB_ENV ;;
-            pfth) echo "PATH_ENV=AP_Region/TH/" >> $GITHUB_ENV ;;
-            pfsg) echo "PATH_ENV=AP_Region/SG/" >> $GITHUB_ENV ;;
-            pfhk) echo "PATH_ENV=AP_Region/HK/" >> $GITHUB_ENV ;;
-            pfau) echo "PATH_ENV=AP_Region/AU/" >> $GITHUB_ENV ;;
-            pfbp) echo "PATH_ENV=AP_Region/BP/" >> $GITHUB_ENV ;;
-            omau) echo "PATH_ENV=AP_Region/OMAU/" >> $GITHUB_ENV ;;
-            pfca) echo "PATH_ENV=CA_Region/CA/" >> $GITHUB_ENV ;;
-            omca) echo "PATH_ENV=CA_Region/OMCA/" >> $GITHUB_ENV ;;
-            pfcl) echo "PATH_ENV=LatAm_Region/CL/" >> $GITHUB_ENV ;;
-            pfco) echo "PATH_ENV=LatAm_Region/CO/" >> $GITHUB_ENV ;;
-            pfpr) echo "PATH_ENV=LatAm_Region/PR/" >> $GITHUB_ENV ;;
-            pfbr) echo "PATH_ENV=LatAm_Region/BR/" >> $GITHUB_ENV ;;
-            pfmx) echo "PATH_ENV=LatAm_Region/MX/" >> $GITHUB_ENV ;;
-            pfxu) echo "PATH_ENV=US_Region/USXU/" >> $GITHUB_ENV ;;
-            pfus) echo "PATH_ENV=US_Region/US/" >> $GITHUB_ENV ;;
-            pffi) echo "PATH_ENV=EURO_Region/FI/" >> $GITHUB_ENV ;;
-            pfra) echo "PATH_ENV=EURO_Region/RA/" >> $GITHUB_ENV ;;
-            pfgb) echo "PATH_ENV=EURO_Region/GB/" >> $GITHUB_ENV ;;
-            pfpt) echo "PATH_ENV=EURO_Region/PT/" >> $GITHUB_ENV ;;
-            pfes) echo "PATH_ENV=EURO_Region/ES/" >> $GITHUB_ENV ;;
-            pffr) echo "PATH_ENV=EURO_Region/FR/" >> $GITHUB_ENV ;;
-            pfde) echo "PATH_ENV=EURO_Region/DE/" >> $GITHUB_ENV ;;
-            pfch) echo "PATH_ENV=EURO_Region/CH/" >> $GITHUB_ENV ;;
-            pfit) echo "PATH_ENV=EURO_Region/IT/" >> $GITHUB_ENV ;;
-            *)
-              echo "Invalid environment: $ENV"
-              exit 1
-              ;;
-          esac
+            # Set ADMIN_USR
+            case "$ENV" in
+              pfnz|pfid|pfph|pfth|pfsg|pfhk|pfau|pfbp|omau)
+                ADMIN_USR=pfmgrap
+                ;;
+              pfca|omca|pfcl|pfco|pfpr|pfbr|pfmx|pfxu|pfus)
+                ADMIN_USR=pfmgram
+                ;;
+              pffi|pfra|pfgb|pfpt|pfes|pffr|pfde|pfch|pfit)
+                ADMIN_USR=pfmgreu
+                ;;
+              *)
+                echo "Invalid environment: $ENV"
+                exit 1
+                ;;
+            esac
 
-      - name: Debug Variables
-        run: |
-          echo "Environment: ${{ matrix.environment }}"
-          echo "Admin User: ${{ env.ADMIN_USR }}"
-          echo "Selected Path: $PATH_ENV"
+            # Set PATH_ENV
+            case "$ENV" in
+              pfnz) PATH_ENV=AP_Region/NZ/ ;;
+              pfid) PATH_ENV=AP_Region/ID/ ;;
+              pfph) PATH_ENV=AP_Region/PH/ ;;
+              pfth) PATH_ENV=AP_Region/TH/ ;;
+              pfsg) PATH_ENV=AP_Region/SG/ ;;
+              pfhk) PATH_ENV=AP_Region/HK/ ;;
+              pfau) PATH_ENV=AP_Region/AU/ ;;
+              pfbp) PATH_ENV=AP_Region/BP/ ;;
+              omau) PATH_ENV=AP_Region/OMAU/ ;;
+              pfca) PATH_ENV=CA_Region/CA/ ;;
+              omca) PATH_ENV=CA_Region/OMCA/ ;;
+              pfcl) PATH_ENV=LatAm_Region/CL/ ;;
+              pfco) PATH_ENV=LatAm_Region/CO/ ;;
+              pfpr) PATH_ENV=LatAm_Region/PR/ ;;
+              pfbr) PATH_ENV=LatAm_Region/BR/ ;;
+              pfmx) PATH_ENV=LatAm_Region/MX/ ;;
+              pfxu) PATH_ENV=US_Region/USXU/ ;;
+              pfus) PATH_ENV=US_Region/US/ ;;
+              pffi) PATH_ENV=EURO_Region/FI/ ;;
+              pfra) PATH_ENV=EURO_Region/RA/ ;;
+              pfgb) PATH_ENV=EURO_Region/GB/ ;;
+              pfpt) PATH_ENV=EURO_Region/PT/ ;;
+              pfes) PATH_ENV=EURO_Region/ES/ ;;
+              pffr) PATH_ENV=EURO_Region/FR/ ;;
+              pfde) PATH_ENV=EURO_Region/DE/ ;;
+              pfch) PATH_ENV=EURO_Region/CH/ ;;
+              pfit) PATH_ENV=EURO_Region/IT/ ;;
+              *)
+                echo "Invalid environment: $ENV"
+                exit 1
+                ;;
+            esac
 
-      - name: Set Environment Variables
-        run: |
-          echo 'ORG_PATH="/home/jenscm1/actions-runner/_work/bcc-pat/bcc-pat/"' >> $GITHUB_ENV
-          echo "TMP_PATH=/tmp/pat/" >> $GITHUB_ENV
+            echo "Admin User: $ADMIN_USR"
+            echo "PATH_ENV: $PATH_ENV"
 
-      - name: Set Permissions for Temporary Path
-        run: |
-          chmod -R 755 $TMP_PATH
+            ORG_PATH="/home/jenscm1/actions-runner/_work/bcc-pat/bcc-pat/"
+            TMP_PATH=/tmp/pat/
+            chmod -R 755 $TMP_PATH
 
-      - name: Set Variables
-        run: |
-          echo "REMOTE_PATH=/home/bambdev1/actions-runner/_work/bcc-pat/bcc-pat/unix/PFW_UNIX/dataimport_pat_pg/opt_64/dataimport_pat_pg" >> $GITHUB_ENV
-          echo "REMOTE_PATH1=/home/bambdev1/actions-runner/_work/bcc-pat/bcc-pat/unix/PFW_UNIX/plsegfix_pat_pg/opt_64/plsegfix_pat_pg" >> $GITHUB_ENV
-          echo "REMOTE_PATH2=/home/bambdev1/actions-runner/_work/bcc-pat/bcc-pat/unix/PFW_UNIX/plsegchk_pat_pg/opt_64/plsegchk_pat_pg" >> $GITHUB_ENV
-          echo "REMOTE_PATH3=/home/bambdev1/actions-runner/_work/bcc-pat/bcc-pat/unix/PFW_UNIX/plstatus_pat_pg/opt_64/plstatus_pat_pg" >> $GITHUB_ENV
-          echo "REMOTE_PATH4=/home/bambdev1/actions-runner/_work/bcc-pat/bcc-pat/unix/PFW_UNIX/validator_pat_pg/opt_64/validator_pat_pg" >> $GITHUB_ENV
-          echo "REMOTE_PATH5=/home/bambdev1/actions-runner/_work/bcc-pat/bcc-pat/unix/PFW_UNIX/lz4Compress/opt_64/lz4Compress" >> $GITHUB_ENV
-          echo "LOCAL_PATH=/tmp/actions/" >> $GITHUB_ENV
+            REMOTE_PATHS=(
+              /home/bambdev1/actions-runner/_work/bcc-pat/bcc-pat/unix/PFW_UNIX/dataimport_pat_pg/opt_64/dataimport_pat_pg
+              /home/bambdev1/actions-runner/_work/bcc-pat/bcc-pat/unix/PFW_UNIX/plsegfix_pat_pg/opt_64/plsegfix_pat_pg
+              /home/bambdev1/actions-runner/_work/bcc-pat/bcc-pat/unix/PFW_UNIX/plsegchk_pat_pg/opt_64/plsegchk_pat_pg
+              /home/bambdev1/actions-runner/_work/bcc-pat/bcc-pat/unix/PFW_UNIX/plstatus_pat_pg/opt_64/plstatus_pat_pg
+              /home/bambdev1/actions-runner/_work/bcc-pat/bcc-pat/unix/PFW_UNIX/validator_pat_pg/opt_64/validator_pat_pg
+              /home/bambdev1/actions-runner/_work/bcc-pat/bcc-pat/unix/PFW_UNIX/lz4Compress/opt_64/lz4Compress
+            )
+            LOCAL_PATH=/tmp/actions/
 
-      - name: Copy Binary Files from Remote to Local
-        run: |
-          for REMOTE in $REMOTE_PATH $REMOTE_PATH1 $REMOTE_PATH2 $REMOTE_PATH3 $REMOTE_PATH4 $REMOTE_PATH5; do
-            scp -r -o StrictHostKeyChecking=no -i ~/.ssh/vscode bambdev1@daylnxcpsd004:$REMOTE $LOCAL_PATH || echo "Skipping $REMOTE due to permission error"
+            for REMOTE in "${REMOTE_PATHS[@]}"; do
+              scp -r -o StrictHostKeyChecking=no -i ~/.ssh/vscode bambdev1@daylnxcpsd004:$REMOTE $LOCAL_PATH || echo "Skipping $REMOTE due to permission error"
+            done
+
+            ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "chmod 750 /$ENV/bin"
+            ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "chmod -R 750 /$ENV/bin/* || true"
+            ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "cp /tmp/actions/* /$ENV/bin/"
+            ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "find ${ORG_PATH}ETL/config_pg/Production/${PATH_ENV} -type f -exec cp {} /$ENV/bin/etl/config_pg/ \;"
+            ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "cp -rf ${ORG_PATH}ETL/scripts_pg/* /$ENV/bin/etl/scripts_pg/"
+            ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "cp -rf ${ORG_PATH}ETL/sql_pg/* /$ENV/bin/etl/sql_pg/"
+            ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "cp -rf ${ORG_PATH}ETL/ref_pg/* /$ENV/bin/etl/ref_pg/"
+            ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "cp -rf ${ORG_PATH}unix/PFW_UNIX/shellscripts_pg/* /$ENV/bin/"
+            ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "chmod -R 750 /$ENV/bin/* || true"
+            ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "chmod -R 550 /$ENV/bin/* || true"
+            ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "chgrp -R $ENV /$ENV/bin/* || true"
+            ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "chmod -R 550 /$ENV/bin || true"
+
+            echo "✅ Completed environment: $ENV"
+            echo "==============================="
+
           done
-
-      - name: Execute SSH Commands
-        env:
-          ADMIN_USR: ${{ env.ADMIN_USR }}
-        run: |
-          ENV="${{ matrix.environment }}"
-          echo "${ADMIN_USR}"
-          echo "$ENV"
-          ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "chmod 750 /$ENV/bin"
-          ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "chmod -R 750 /$ENV/bin/* || true"
-          ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "cp /tmp/actions/* /$ENV/bin/"
-          ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "find ${ORG_PATH}ETL/config_pg/Production/${PATH_ENV} -type f -exec cp {} /$ENV/bin/etl/config_pg/ \;"
-          ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "cp -rf ${ORG_PATH}ETL/scripts_pg/* /$ENV/bin/etl/scripts_pg/"
-          ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "cp -rf ${ORG_PATH}ETL/sql_pg/* /$ENV/bin/etl/sql_pg/"
-          ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "cp -rf ${ORG_PATH}ETL/ref_pg/* /$ENV/bin/etl/ref_pg/"
-          ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "cp -rf ${ORG_PATH}unix/PFW_UNIX/shellscripts_pg/* /$ENV/bin/"
-          ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "chmod -R 750 /$ENV/bin/* || true"
-          ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "chmod -R 550 /$ENV/bin/* || true"
-          ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "chgrp -R $ENV /$ENV/bin/* || true"
-          ssh -o StrictHostKeyChecking=no -i ~/.ssh/vscode "${ADMIN_USR}@daylnxcpsp014.enterprisenet.org" "chmod -R 550 /$ENV/bin || true"
