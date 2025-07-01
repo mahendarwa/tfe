5d6ef69315594f1bb27c8df8a101126081d2f76c
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-07-01 23:01:14 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,6 +1,6 @@
 sudo systemctl daemon-reexec
 sudo systemctl daemon-reload
-sudo systemctl start actions.runner.zilvertonz-GBS_DAE_Python_ETL.hdclappxd135.service
-sudo systemctl enable actions.runner.zilvertonz-GBS_DAE_Python_ETL.hdclappxd135.service
-sudo systemctl status actions.runner.zilvertonz-GBS_DAE_Python_ETL.hdclappxd135.service
-journalctl -u actions.runner.zilvertonz-GBS_DAE_Python_ETL.hdclappxd135.service -f
+sudo systemctl start actions.runner.zilvertonz-GBS_DAE_Devops_Automation.hdclappxd135.service
+sudo systemctl enable actions.runner.zilvertonz-GBS_DAE_Devops_Automation.hdclappxd135.service
+sudo systemctl status actions.runner.zilvertonz-GBS_DAE_Devops_Automation.hdclappxd135.service
+journalctl -u actions.runner.zilvertonz-GBS_DAE_Devops_Automation.hdclappxd135.service -f
