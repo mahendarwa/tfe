b41d31f1927db19fc91db42e67659a3329ba4874
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-07-01 23:58:58 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,10 +1,12 @@
-# Stop and uninstall the existing service
+cd ~/actions-runner
+
+# Stop and uninstall old runner
 sudo ./svc.sh stop
 sudo ./svc.sh uninstall
 
-# Remove current runner config
-./config.sh remove --token <PASTE_NEW_TOKEN_HERE>
-./config.sh --url https://github.com/zilvertonz/GBS_DAE_Devops_Automation --token <PASTE_NEW_TOKEN_HERE>
+# Delete the old config (skip `config.sh remove`)
+rm -rf .runner
+./config.sh --url https://github.com/zilvertonz/GBS_DAE_Devops_Automation --token <new_token>
 sudo ./svc.sh install
 sudo systemctl start actions.runner.zilvertonz-GBS_DAE_Devops_Automation.hdclappxd135.service
 sudo systemctl enable actions.runner.zilvertonz-GBS_DAE_Devops_Automation.hdclappxd135.service
