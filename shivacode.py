ba20a5a6cce780784d97bb396b7a2d526e12ff7d
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-07-04 00:02:26 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,3 +1,18 @@
-Just to confirm — should all GKE clusters be configured with the maintenance window 2:00–4:00 AM UTC, weekly on Saturday?
+  env:
+    TIME_ZONE: ${{ inputs.time_zone }}
+timezone = os.getenv("TIME_ZONE", "CST") 
 
-Also, should the Rego policy match this exact window, or is some variation acceptable?
+
+if timezone.upper() == "EST":
+    set_timezone = "SET TIME ZONE 'america eastern';"
+else:
+    set_timezone = "SET TIME ZONE 'america central';"
+
+
+bteq_content = f"""
+.LOGON {host}/{user},Rp$QC$c_4dw;
+{set_timezone}
+COMPILE FILE = {temp_proc_path};
+.LOGOFF;
+.QUIT;
+"""
