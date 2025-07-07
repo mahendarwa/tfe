f709098eab268fd33911bf572d6122f7e2ddd5ce
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-07-08 00:28:42 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,18 +1,8 @@
-  env:
-    TIME_ZONE: ${{ inputs.time_zone }}
-timezone = os.getenv("TIME_ZONE", "CST") 
+package gke.maintenance
 
+default compliant = false
 
-if timezone.upper() == "EST":
-    set_timezone = "SET TIME ZONE 'america eastern';"
-else:
-    set_timezone = "SET TIME ZONE 'america central';"
-
-
-bteq_content = f"""
-.LOGON {host}/{user},Rp$QC$c_4dw;
-{set_timezone}
-COMPILE FILE = {temp_proc_path};
-.LOGOFF;
-.QUIT;
-"""
+compliant {
+    input.maintenancePolicy.window.dailyMaintenanceWindow.startTime
+    input.maintenancePolicy.window.dailyMaintenanceWindow.duration
+}
