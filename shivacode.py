97621ca3deba76ffb7c0017c5b7a7e9e36f65942
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-07-29 00:03:31 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,1 +1,16 @@
-Invoke-WebRequest -Uri 'https://auth.app.wiz.io/api/token/device' -Method POST -UseBasicParsing
+$deviceCode = '<YOUR_DEVICE_CODE>'
+
+$headers = @{
+    'Content-Type' = 'application/x-www-form-urlencoded'
+}
+
+$body = "device_code=$deviceCode"
+
+$response = Invoke-RestMethod `
+  -Uri 'https://auth.app.wiz.io/api/token/device' `
+  -Method POST `
+  -Headers $headers `
+  -Body $body `
+  -UseBasicParsing
+
+$response | ConvertTo-Json -Depth 10
