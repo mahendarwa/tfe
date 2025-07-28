246bf2d9745cf21f330c5a28d7893066e3ca245a
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-07-28 23:14:43 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,33 +1,7 @@
-name: Merge Feature Branch into Master
+You’re setting first: 3, after: 3, but after should be set to the actual endCursor value (a string), not a number.
 
-on:
-  workflow_dispatch:
-    inputs:
-      feature_branch:
-        description: 'Name of the feature branch to merge into master'
-        required: true
 
-jobs:
-  merge:
-    runs-on: zilverton-private-x64-ubuntu
-
-    steps:
-      - name: Checkout GBS_DAE_OSS repository
-        uses: actions/checkout@v2
-        with:
-          repository: 'zilvertonz/GBS_DAE_OSS'
-          token: ${{ secrets.MY_GITHUB_TOKEN }}
-          ref: 'master'
-          path: 'GBS_DAE_OSS'
-          fetch-depth: 0
-
-      - name: Merge feature branch into master
-        run: |
-          cd GBS_DAE_OSS
-          git config --global user.email "a@cignahealthcare.com"
-          git config --global user.name "C8230G_Zilver"
-          git checkout master
-          git pull origin master
-          git fetch origin ${{ github.event.inputs.feature_branch }}
-          git merge origin/${{ github.event.inputs.feature_branch }} --no-ff -m "Merge feature branch ${{ github.event.inputs.feature_branch }} into master"
-          git push origin master
+graphSearch(
+  first: $batch,
+  after: "$endCursor"
+)
