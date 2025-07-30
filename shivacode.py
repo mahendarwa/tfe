70861b0b9f83d05e7230dbda5fad6c6b188d86b6
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-07-31 00:32:15 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,27 +1,11 @@
 package wiz
 
-allowed_kinds := {
-    "ReplicaSet",
-    "DaemonSet",
-    "CronJob",
-    "StatefulSet",
-    "Deployment",
-    "Job"
-}
-
 default result := "pass"
 
-# Fail if ownerReferences is missing
+# Fail if ownerReferences is missing or empty
 result := "fail" {
     not input.object.metadata.ownerReferences
 }
 
-# Fail if any ownerReference.kind is NOT in allowed list
-result := "fail" {
-    some i
-    input.object.metadata.ownerReferences[i].kind
-    not input.object.metadata.ownerReferences[i].kind in allowed_kinds
-}
-
 currentConfiguration := sprintf("%v", [input.object.metadata.ownerReferences])
-expectedConfiguration := "Pods must be managed by one of: ReplicaSet, DaemonSet, CronJob, StatefulSet, Deployment, Job"
+expectedConfiguration := "Pods must have ownerReferences"
