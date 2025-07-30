cc032ccffc38ffa70130b7d3812c973521efc841
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-07-31 00:28:43 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -11,13 +11,16 @@ allowed_kinds := {
 
 default result := "pass"
 
-
+# Fail if ownerReferences is missing
 result := "fail" {
-    not input.spec.ownerReferences
-} else := "fail" {
-    some i
-    not input.spec.ownerReferences[i].kind in allowed_kinds
+    not input.object.metadata.ownerReferences
 }
 
-currentConfiguration := sprintf("%v", [input.spec.ownerReferences])
+# Fail if any ownerReference kind is not allowed
+result := "fail" {
+    some i
+    not input.object.metadata.ownerReferences[i].kind in allowed_kinds
+}
+
+currentConfiguration := sprintf("%v", [input.object.metadata.ownerReferences])
 expectedConfiguration := "Pods must be managed by one of: ReplicaSet, DaemonSet, CronJob, StatefulSet, Deployment, Job"
