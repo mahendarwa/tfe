d8246316b6b214e1c6045b82139ba23ba0690edc
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-08-07 11:47:14 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,1 +1,1 @@
-sudo rsync -av --delete --no-o --no-g --no-perms --no-xattrs /opt/versions/python/ /pythonPROD/python/
+sudo rsync -av --relative --delete --no-o --no-g --no-perms --no-xattrs /opt/versions/python/./ /pythonPROD/python/
