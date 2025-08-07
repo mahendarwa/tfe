ff7835ca6383521363ed4cd2bdacfc6326b8c469
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-08-07 23:02:03 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,5 +1,45 @@
-cd /opt/versions
-sudo tar -czf /tmp/python-etl.tar.gz python
-sudo cp /tmp/python-etl.tar.gz /pythonPROD/
-cd /pythonPROD/
-sudo tar -xzf python-etl.tar.gz
+package wiz
+
+default result = "skip"
+
+result = "skip" {
+  input.object.kind != "Gateway"
+}
+
+result = "skip" {
+  not contains(input.object.apiVersion, "istio.io")
+}
+
+result = "pass" {
+  count(invalid_hosts) == 0
+}
+
+result = "fail" {
+  count(invalid_hosts) > 0
+}
+
+invalid_hosts := {host |
+  some i
+  host := input.object.spec.servers[i].hosts[_]
+
+  # Reject if host doesn't start with expected namespace or "./"
+  not startswith(host, concat("/", [input.namespace, ""]))
+  not startswith(host, "./")
+
+  # OR reject if host has wildcard "*" after namespace (e.g., "namespace/*")
+} union {host |
+  some i
+  server := input.object.spec.servers[i]
+  some j
+  host := server.hosts[j]
+  parts := split(host, "/")
+  count(parts) > 1
+  startswith(parts[1], "*")
+}
+
+startswith(str, prefix) = true {
+  substring(str, 0, count(prefix)) == prefix
+}
+
+currentConfiguration := sprintf("Host values: %v", [input.object.spec.servers[_].hosts])
+expectedConfiguration := "Host values should not start with '*' after 'namespace/'"
