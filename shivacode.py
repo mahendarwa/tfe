151a6ae5decd047668461fec1e4c7aadce51ae0a
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-08-07 12:55:38 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,3 +1,2 @@
-sudo mkdir -p /pythonPROD/python/scripts/file_movement_pattern
-sudo mkdir -p /pythonPROD/python/scripts/file_movement_pattern/{dev,prod,test}
-sudo rsync -av --delete --no-o --no-g --no-perms --no-xattrs /opt/versions/python/./ /pythonPROD/python/
+sudo mkdir -p /pythonPROD/python
+sudo cp -R /opt/versions/python/ /pythonPROD/
