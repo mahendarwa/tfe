f6b50b0ccbb2b811552dc53e558d98da90137b3f
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-08-07 11:39:32 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,7 +1,1 @@
-- name: copy python files python-etl code to /opt/pythonetl folder using rsync
-  ansible.builtin.shell: "sudo rsync -av --delete /opt/versions/python/ /python{{ env }}/python/"
-  become: true
-  retries: 3
-  delay: 3
-  register: result_8
-  until: result_8 is succeeded
+sudo rsync -av --delete /opt/versions/python/ /pythonPROD/python/
