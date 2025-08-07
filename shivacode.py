71ba6fe9b29b82d563e93ab62a084ae1205206cb
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-08-07 11:03:13 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,1 +1,7 @@
-We have observed a recurring pattern where the spike in failed scans is significantly higher every Wednesday. Could you please check if this information can be utilized?
+- name: copy python files python-etl code to /opt/pythonetl folder using rsync
+  ansible.builtin.shell: "sudo rsync -av --delete /opt/versions/python/ /python{{ env }}/python/"
+  become: true
+  retries: 3
+  delay: 3
+  register: result_8
+  until: result_8 is succeeded
