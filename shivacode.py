91603be660f3b9f54705e4cbf9bd8993bbacb462
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-08-15 00:23:56 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,1 +1,4 @@
-final_sql = re.sub(r"\$\{env\.id\}", env_id, final_sql)
+env_id_plain = executionenv if executionenv.upper() in ("DEV2", "DEV", "INT", "QA") else ""
+
+final_sql = re.sub(r"\$\{env\.id\.upper\}", env_id.upper(), content)
+final_sql = re.sub(r"\$\{env\.id\}", env_id_plain, final_sql)
