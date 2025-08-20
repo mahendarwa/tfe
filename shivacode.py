af99f74f7a6679b700a94e1c5ef63ec9f37a691b
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-08-20 16:38:08 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,1 +1,3 @@
-The application remains Unlicensed when opened, and the issue has been outstanding for ~3 weeks, impacting deliverables.
+I noticed that in the “Failed scans by Resource Region” report, the dashboard is still displaying failed scans across earlier dates, even though there were no errors on Aug 18 and Aug 19. The failures shown are from multiple regions, including Azure (CentralUS, EastUS2) and GCP (us-east4).
+
+We observed that there were no errors in the last 5 days, but the dashboard is still showing failed scans for those days now. Could you please confirm why these errors are appearing retroactively when they were not there earlier?
