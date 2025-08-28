69d4c03af34aa8734e852bd3aeb6f622c72b77cb
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-08-28 21:44:50 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,1 +1,17 @@
-cloudLib.isNullOrEmpty(input.bucketEncryptionConfiguration.ServerSideEncryptionConfiguration.Rules[j].ApplyServerSideEncryptionByDefault.KMSMasterKeyID)
+package wiz
+
+import data.generic.cloud as cloudLib
+
+default result = "pass"
+
+result = "fail" {
+    input.bucketEncryptionConfiguration.ServerSideEncryptionConfiguration.Rules[j].BucketKeyEnabled = false
+}
+
+result = "fail" {
+    not cloudLib.isNullOrEmpty(input.bucketEncryptionConfiguration.ServerSideEncryptionConfiguration.Rules[j].ApplyServerSideEncryptionByDefault.KMSMasterKeyID)
+}
+
+result = "fail" {
+    input.bucketEncryptionConfiguration.ServerSideEncryptionConfiguration.Rules[j].ApplyServerSideEncryptionByDefault.SSEAlgorithm != "AES256"
+}
