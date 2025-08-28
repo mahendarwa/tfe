d6f57c8f56eb7b0834ddd69d5fa727e1f571f694
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-08-28 16:54:23 +0530

    Update qa.tf

--- qa.tf
+++ qa.tf
@@ -1,41 +1,21 @@
+cloud engineering fall under ISTS  -- ISTS-dspm-poc
+=====
+HCD
+   cvs-hcd
+    non-cvs-hcd -- aquired copmnaies 
+       Signifyhealth-##RG##    --Azure
+       Oakstreet-healt-##RD##    --Azure(they ve only azure)
+       caravan##RD##   (only azure)
 
-name: Merge feature branch
 
-on:
-  workflow_dispatch:
-    inputs:
-      feature_branch:
-        description: 'Name of the feature branch to merge into master'
-        required: true
+==
+organation path --you have the org ID -- chec in the setting/deployment -cloud
+cloud engineering --  project name# ISTS ##RG
+unassigned subsctiptions- -- copy -- project id 
+go to the project -- manage subction--
 
-jobs:
-  merge:
-    runs-on: ubuntu-latest
-    steps:
-      - name: Checkout GBS_DAE_OSS repository
-        uses: actions/checkout@v3
-        with:
-          repository: 'zilvertorz/GBS_DAE_OSS'
-          token: ${{ secrets.MY_GITHUB_TOKEN }}
-          ref: 'master'
-          path: 'GBS_DAE_OSS'
-          fetch-depth: 0
+GAM project -- subscriopt name -- you can see org path --- signify --come under - HCD(health care delivery)
 
-      - name: Merge feature branch into master
-        run: |
-          cd GBS_DAE_OSS
-          git config --global user.email "abc@cignahealthcare.com"
-          git config --global user.name "CBZ83G_Zilver"
-
-          echo "Switching to master branch..."
-          git checkout master
-          git pull origin master
-
-          echo "Fetching feature branch: ${{ github.event.inputs.feature_branch }}"
-          git fetch origin ${{ github.event.inputs.feature_branch }}
-
-          echo "Merging feature branch into master (auto-resolve using theirs)..."
-          git merge origin/${{ github.event.inputs.feature_branch }} -X theirs --no-edit
-
-          echo "Pushing changes to master..."
-          git push origin master
+go to the project -- add signity -- in hcd GCP RG 
+==
+EDP -- will go into -- projct name -- (Data & Analytics --- EDP##RG##
