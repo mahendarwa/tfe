227cf67644aed29756341977245d7088d51956a0
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-09-10 22:06:07 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,1 +1,11 @@
-I see the below items in the attached sheet from the request. They are slightly different from css-cc-ManagedAcc. Can I go ahead and suppress these? I have already suppressed the issues mentioned in request RITM0902430.
+Re-check exact path
+dir "C:\Users\C879571\IdeaProjects\rebate-rules\frontend\node_modules\@isaacs\cliui\node_modules\ansi-styles" -Recurse
+dir "C:\Users\C879571\IdeaProjects\rebate-rules\frontend\node_modules\error-ex" -Recurse
+
+Search entire drive
+Get-ChildItem -Path C:\Users\C879571\ -Recurse -Include "ansi-styles","error-ex" -ErrorAction SilentlyContinue
+
+Check Windows Installer/Temp
+Get-ChildItem C:\Windows\Installer -Recurse | findstr ansi
+Get-ChildItem $env:TEMP -Recurse | findstr error-ex
+
