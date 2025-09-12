854e46ab277ea4df88fa458204369edb1f1922d7
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-09-12 23:33:25 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -3,38 +3,42 @@ package wiz
 default result = "pass"
 
 result = "fail" {
-    count(invalid_hosts) > 0
-}
-
-
-invalid_hosts[host] {
-    some i
-    host := input.spec.servers[i].hosts[_]
-    not startswith(host, concat("/", [input.metadata.namespace, ""]))
-    not startswith(host, "./")
+  count(invalid_hosts) > 0
 }
 
 invalid_hosts[host] {
-    some i
-    server := input.spec.servers[i]
-    some j
-    host := server.hosts[j]
-    host == "*"
+  some i
+  some j
+  server := input.spec.servers[i]
+  host := server.hosts[j]
+
+  not startswith(host, concat("/", [input.metadata.namespace, ""]))
+  not startswith(host, "./")
 }
 
 invalid_hosts[host] {
-    some i
-    server := input.spec.servers[i]
-    some j
-    host := server.hosts[j]
-    parts := split(host, "/")
-    count(parts) > 1
-    startswith(parts[1], "*")
+  some i
+  some j
+  server := input.spec.servers[i]
+  host := server.hosts[j]
+
+  host == "*"
+}
+
+invalid_hosts[host] {
+  some i
+  some j
+  server := input.spec.servers[i]
+  host := server.hosts[j]
+
+  parts := split(host, "/")
+  count(parts) > 1
+  startswith(parts[1], "*")
 }
 
 startswith(str, prefix) {
-    substring(str, 0, count(prefix)) == prefix
+  substring(str, 0, count(prefix)) == prefix
 }
 
 currentConfiguration := sprintf("Host values: %v", [input.spec.servers[_].hosts])
-expectedConfiguration := "Host values must not be '*' or namespace/*"
+expectedConfiguration := "Host values should not start with '*' or be '*' after namespace"
