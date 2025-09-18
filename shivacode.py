51c88d091117f0dc7062f796761557385501da29
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-09-18 23:21:58 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,1 +1,14 @@
-Just for the record, for server xyz, I noticed that 6 file paths show critical malicious files, while only 5 were listed in the thread. Adding this note here for completeness.
+package wiz
+
+default result = "fail"
+
+result = "pass" {
+    input.tags[_].key.shortName == "costcenter"
+    input.tags[_].values[_].shortName != null
+
+  input.tags[_].key.shortName == "appname"
+    input.tags[_].values[_].shortName != null
+}
+
+currentConfiguration := sprintf("IaC - Project tags does not exist or is empty:", [count(input.tags[_].key)])
+expectedConfiguration := sprintf("IaC - Project tags should not be empty:", [count(input.tags[_].values[_])])
