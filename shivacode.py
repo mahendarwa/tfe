be8f3ca733e3f4fcdf2dd545618f81989a6f7200
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-09-19 15:19:40 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,14 +1,5 @@
-package wiz
+We are from the Wiz Security team, which continuously scans and detects potential vulnerabilities across environments. Recently, Wiz has detected an OpenShift cluster created on-prem.
 
-default result = "fail"
+As part of our project mapping process, we want to add this OpenShift cluster into the respective Wiz project for better visibility and governance. We have received your contact details for this cluster.
 
-result = "pass" {
-    input.tags[_].key.shortName == "costcenter"
-    input.tags[_].values[_].shortName != null
-
-  input.tags[_].key.shortName == "appname"
-    input.tags[_].values[_].shortName != null
-}
-
-currentConfiguration := sprintf("IaC - Project tags does not exist or is empty:", [count(input.tags[_].key)])
-expectedConfiguration := sprintf("IaC - Project tags should not be empty:", [count(input.tags[_].values[_])])
+Could you please help us with the datacenter details related to this OpenShift cluster, so that we can complete the mapping in Wiz?
