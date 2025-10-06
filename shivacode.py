ccb69d10787326e1ec2b888fb0cee8ce56bf99b4
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-10-06 20:01:30 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,7 +1,1 @@
-For subscription [add subscription ID/name here], could you please configure the following:
-
-Enable AKS Kubernetes Audit logs under Diagnostic Settings (to capture who connects to the cluster and related activities).
-
-Route these logs via Event Hub so that Wiz can ingest them.
-
-As confirmed by Wiz support, when AKS diagnostic logs are streamed via Event Hub, the Kubernetes Audit log category provides the required user identity and activity details. Wiz normalizes these fields to give us visibility into actions performed within the AKS environment.
+=IF(C2<>"", LEN(C2)-LEN(SUBSTITUTE(C2,",",""))+1, 0)
