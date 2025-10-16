2b506faa74c5c603fe430351fb74c602b62c6ee4
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-10-16 20:41:10 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,1 +1,2 @@
-Get-Service | Where-Object {$_.Name -like "*splunk*"}
+The ServiceNow request for the policy swap has been approved, and I’ve completed the policy swap activity.
+The new custom policy with the exclusion for disabled accounts is now active in Wiz.
