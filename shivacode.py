55c7f1f7ba30f0beb4e5e1895eef7711228ab0b4
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-10-16 21:07:35 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,2 +1,4 @@
-The ServiceNow request for the policy swap has been approved, and I’ve completed the policy swap activity.
-The new custom policy with the exclusion for disabled accounts is now active in Wiz.
+As per your recommendation, we implemented the exclusion for AWSReservedSSO and CSS accounts in our custom policy. However, the issue count dropped from 2691 to 166, which seems too high a reduction.
+We validated that only about 39 accounts are actually AWSReservedSSO or CSS — so the overall count should reduce by around 39, not more.
+
+Could you please review the logic used in the exclusion filter and confirm if it’s applied correctly?
