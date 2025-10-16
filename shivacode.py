1223eaaaa81e979c76d005cf7ccb78ec514eb346
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-10-16 19:20:37 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,8 +1,1 @@
-Frequent VM Scan Failures – Error Code Internal-J002 (Azure EastUS2 Region)
-
-Body:
-Hi Wiz Support Team,
-
-We’ve observed multiple VM workload scans failing across the Azure EastUS2 region. All affected resources show the same error message — “Internal-J002” — under the scan logs (screenshot attached).
-
-Could you please help us understand the root cause of these Internal-J002 failures and advise any remediation or configuration changes required on our end?
+Get-Service | Where-Object {$_.Name -like "*splunk*"}
