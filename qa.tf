5af406a4a69bc1ae87634873b2c7c158049591ba
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-10-29 18:52:30 +0530

    Update qa.tf

--- qa.tf
+++ qa.tf
@@ -1,1 +1,1 @@
-I’d like to recognize Vinay from the Architecture team for his quick doubt clearing and decision-making, and Manas, Siva, and Sonali for their valuable contributions and support.
+dotnet list package | findstr Newtonsoft.Json
