f1a781d96ba698834f67c6e146f2bedae82dbd0c
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-10-29 18:55:39 +0530

    Update qa.tf

--- qa.tf
+++ qa.tf
@@ -1,1 +1,11 @@
-dotnet list package | findstr Newtonsoft.Json
+Get-ChildItem -Path "C:\Program Files\WindowsPowerShell\Modules" -Recurse -Filter Newtonsoft.Json.dll -ErrorAction SilentlyContinue |
+Select-Object FullName, @{Name='Version';Expression={(Get-Item $_.FullName).VersionInfo.FileVersion}}
+
+
+(Get-Item "C:\Program Files\WindowsPowerShell\Modules\Microsoft.Graph.Devices.CorporateManagement\1.9.3\bin\Newtonsoft.Json.dll").VersionInfo.FileVersion
+
+Get-Module -ListAvailable Microsoft.Graph* | Select Name, Version
+
+
+Update-Module Microsoft.Graph
+Install-Module Microsoft.Graph -Force
