1a9d6f8d8b463c4460b3109c160be7eeeedf86f1
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-10-31 18:01:12 +0530

    Update qa.tf

--- qa.tf
+++ qa.tf
@@ -1,1 +1,5 @@
-the Wiz application is responding very slowly today, Please investigate the cause of the slow response and confirm if there is any ongoing backend issue or performance degradation on the Wiz side.
+Thanks for your earlier response. We did some additional review and wanted to provide more context.
+
+The AKS login events are being captured at the subscription level through Azure Activity Logs (Event Hub 1). These reflect actions such as when a user accesses the AKS cluster from the console.
+
+Once the user connects via kubectl exec and performs in-cluster actions (e.g., file creation or deletion), those are logged separately at the cluster level (Event Hub 2). Since these logs originate from two different Event Hubs, they currently don’t get correlated under a single event chain.
