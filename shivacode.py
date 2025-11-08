360347b5cc53db7f841eee384a2af21805b5ae1a
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-11-08 16:51:35 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -2,3 +2,24 @@
 
 
 detect-unauthorized-hostpath-access-except-o11y
+
+
+apiVersion: v1
+kind: Pod
+metadata:
+  name: fim-test-default
+  namespace: default
+spec:
+  containers:
+  - name: test
+    image: busybox
+    command: ["/bin/sh", "-c", "sleep 3600"]
+    volumeMounts:
+    - name: varlog
+      mountPath: /var/log/pods
+      readOnly: true
+  volumes:
+  - name: varlog
+    hostPath:
+      path: /var/log/pods
+      type: Directory
