ee287fd6ad1ae1344594a403194d2884b838dba4
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-11-17 16:53:00 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,1 +1,5 @@
-Apologies for the delay in getting back. We’re currently reviewing the best possible approach to build the required view and will provide an update once validated.
+we have these some of the information on tablea captured already through the ingestion from wiz weekly basis and send out score cards,
+and let us  know if you are not part those emails.we can add you there so that you will received this email weekly basis.
+we can also give the access to the tableau and you access this and download specific to your LOB or cloud provide specific.
+
+if you have any question, we can schedule sometime with Satish 
