01f715354d12d1b703841e797c29dd31b5524733
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-11-26 21:46:17 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,3 +1,1 @@
-Thanks, Will.
-Right — for the six queries you checked, we don’t have any violations, so that part looks fine.
-For the other few policies, we do have violations, and we’ll take those forward accordingly.
+pg_isready -h 10.223.252.133 -p 5432 -U csspgspl_user -d csspgspl_db
