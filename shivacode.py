e46be5eb3370ac8b77489b68e53dae2a7c14f525
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-12-11 00:19:24 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,2 +1,2 @@
-Messages are reaching Teams, but the Adaptive Card formatting is not rendering correctly — the payload is posting as raw JSON. I’ve followed the steps and added the action templates, but the output is still not structured.
-A quick call would help resolve this faster.
+docker pull rduscedsacr.azurecr.io/<repository>/<tag>
+curl -v https://rduscedsacr.azurecr.io/v2/_catalog
