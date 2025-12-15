3718e43a72beb9d54169a302503fde1df14a5074
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-12-15 22:52:38 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,3 +1,1 @@
-he mentioned resources in the ticket have been taken care of and are now exempted from the reported issues.
-
-Please let me know if anything further is required.
+item()?['content']
