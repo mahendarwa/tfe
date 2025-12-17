08c656556eda46854034f8d71f3a6c37f036259c
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-12-17 19:10:55 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,2 +1,10 @@
-body('Parse_JSON')?['integrationAction']?['params']?['body']?['attachments']
-item()?['content']
+Validation Request – Wiz CCR Promotion to Prod (RITM1837181)
+
+Hi Adam,
+
+As per request RITM1837181 from Will, I have moved the below Wiz CCR from Dev to Prod and temporarily set the severity to INFO.
+
+Could you please validate and confirm so that we can proceed with promoting it to the final confirmed severity?
+
+CCR Name:
+CVS Custom – Unapproved AI Models
