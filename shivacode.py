df7307b38fd4beba0ef686c2d2413380e13ca9ae
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2025-12-18 19:56:52 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,10 +1,2 @@
-Validation Request – Wiz CCR Promotion to Prod (RITM1837181)
-
-Hi Adam,
-
-As per request RITM1837181 from Will, I have moved the below Wiz CCR from Dev to Prod and temporarily set the severity to INFO.
-
-Could you please validate and confirm so that we can proceed with promoting it to the final confirmed severity?
-
-CCR Name:
-CVS Custom – Unapproved AI Models
+e Wiz platform admin, I don’t have OS-level access to manage local VM users.
+The password update for the local user must be completed by the respective Azure VM/application owner, after which we’ll validate closure in Wiz.
