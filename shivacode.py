d14e6b38ecba4a7d46f46762b9f052c15045c7e3
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2026-01-02 17:55:25 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,3 +1,3 @@
-As the Wiz platform admin, I don’t have access to manage local OS users on the VMs. The remediation requires either removing the local VM user if it’s no longer needed, or rotating the password to meet CVS complexity standards, which should be handled by the OS or application owner team.
-
-Once completed, we’ll validate closure via a Wiz rescan.
+triggerOutputs()?['body']?['attachments']
+triggerBody()?['attachments']
+triggerOutputs()?['body']?['body']?['attachments']
