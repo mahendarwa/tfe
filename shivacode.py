637d0017c4e1e56cc585d23e503c0a6cc797bb8e
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2026-02-05 22:34:18 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,3 +1,3 @@
-Scheduling Wiz reports is not possible at the moment due to limits.
-Your role has been updated with Project Boards creation access — please refresh and try sending the reports to the required teams.
-You may need to set up some automation for recurring reportin
+I noticed that one of the ITPRs expected for the current time period is not visible in Clarity PPM and does not appear to be allocated to me.
+
+Could you please check and confirm if the ITPR is pending allocation or assign it so I can proceed with time entry?
