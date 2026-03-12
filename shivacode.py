1a62e45e81b5429178b6de8e181525842a261353
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2026-03-12 21:21:03 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,3 +1,1 @@
-A previous request to grant membership in Account Group CLGRP-Wiz-PCW-RO (Azure Prod Tenant) for Rakesh Neelarapu shows as fulfilled, but the access is not reflected in Entra AD.
-
-To resolve the automation workflow issue, please revoke the existing access for this entitlement so that we can submit a fresh Add Access request again.
+MITRE ATT&CK Cloud Matrix is the official MITRE framework specifically designed for cloud environments such as AWS, Azure, and GCP. It maps real-world cloud attack techniques and helps security tools like Wiz categorize and detect cloud threats effectively.
