cfdc06c7900812788dfe5f0cc1a0519aee6c0792
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2026-03-12 21:24:12 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,1 +1,3 @@
-MITRE ATT&CK Cloud Matrix is the official MITRE framework specifically designed for cloud environments such as AWS, Azure, and GCP. It maps real-world cloud attack techniques and helps security tools like Wiz categorize and detect cloud threats effectively.
+MITRE ATT&CK Cloud Matrix is the official MITRE framework specifically designed for cloud environments such as AWS, Azure, and GCP.
+The 190 cloud techniques are basically a subset of the 912 enterprise techniques.
+The Cloud Matrix only contains techniques relevant to cloud platforms (IaaS, SaaS, Identity providers, Office 365, etc.), while the Enterprise matrix contains all techniques across Windows, Linux, macOS, containers, network devices, and cloud.
