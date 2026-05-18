348a7aa4cc36c41e1c90ff3837c797535822b732
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2026-05-18 22:43:26 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,3 +1,5 @@
 https://certified.training.wiz.io/8c0ce5f1-b4a7-483a-9841-79f2fee5aa8e?utm_source=whatsapp&utm_medium=social
 
 https://certified.training.wiz.io/8c0ce5f1-b4a7-483a-9841-79f2fee5aa8e#acc.qgL2FoVN
+
+I’m happy to share that I have successfully cleared the Wiz Certified Cloud User (WCCU) certification.
