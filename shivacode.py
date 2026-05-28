4b2086f122349537441ee6f7c38566fb98e82f03
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2026-05-28 21:22:54 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,2 +1,23 @@
-the Service Principal is created successfully, but I could not find the CR-DataFactory-Deployment role assigned in IAM.
-Could you please help who can assist on this role assignment?
+Function App Details:
+Function App Name: func-sub-corp-eisdev-use2-adfdeploy
+Subscription: SUB-CORP-EISDEV
+Resource Group: rg-sub-corp-eisdev-nonprod-use2-app
+Region: East US 2
+Runtime Stack: Python 3.11
+Operating System: Linux
+Hosting Plan: Consumption
+Storage Account: stsubcorpeisdevfunc01
+Application Insights: appi-sub-corp-eisdev-use2
+Managed Identity: System Assigned
+Public Network Access: Enabled
+
+Purpose:
+Required for ADF pipeline deployment, automation, and GitHub Actions integration.
+
+
+
+        Please grant Website Contributor role at RG level for rg-corp-use2-snap-prod to access Function App config/app settings for fa-corp-use2-snap-prod
+                                                                                                                               
+Role: Website Contributor
+Scope: Resource Group level
+Resource Group: rg-corp-use2-snap-prod
