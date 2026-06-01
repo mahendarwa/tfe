21b10ee0f8356d1d50845b01aa12625a38fdc7e1
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2026-06-01 16:38:11 +0530

    Update shivacode.py

--- shivacode.py
+++ shivacode.py
@@ -1,23 +1,3 @@
-Function App Details:
-Function App Name: func-sub-corp-eisdev-use2-adfdeploy
-Subscription: SUB-CORP-EISDEV
-Resource Group: rg-sub-corp-eisdev-nonprod-use2-app
-Region: East US 2
-Runtime Stack: Python 3.11
-Operating System: Linux
-Hosting Plan: Consumption
-Storage Account: stsubcorpeisdevfunc01
-Application Insights: appi-sub-corp-eisdev-use2
-Managed Identity: System Assigned
-Public Network Access: Enabled
+I have raised the request with “Yes” selected for Client ID and Secret for the below Service Principal and would like to proceed with onboarding. Kindly help share the client secret for the below Service Principal.
 
-Purpose:
-Required for ADF pipeline deployment, automation, and GitHub Actions integration.
-
-
-
-        Please grant Website Contributor role at RG level for rg-corp-use2-snap-prod to access Function App config/app settings for fa-corp-use2-snap-prod
-                                                                                                                               
-Role: Website Contributor
-Scope: Resource Group level
-Resource Group: rg-corp-use2-snap-prod
+Service Principal: sp-sssharedservices-wizcloudsecurityplatform-dev
